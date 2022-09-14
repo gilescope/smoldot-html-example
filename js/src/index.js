27a6677d1204696fa7551b8d6b8ec3f215c98ce8
Giles Cope <gilescope@gmail.com>
2022-09-14 20:51:19 +0100

    I hate js modules.

--- js/src/index.js
+++ js/src/index.js
@@ -1,31 +1,26 @@
 // import { smoldot } from './js/smoldot-bundled-browserify.js';
+
 let current;
 
 const client = smoldot.start();
 window.client = client;
 
-function load(url, callback) {
-    var xhr = new XMLHTTPRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) callback(xhr.responseText);
-    };
-    xhr.open("GET", url, true);
+console.log("loaded chainspec");
+if (!window.hasOwnProperty('wasm'))
+{
+    await new Promise(r => setTimeout(r, 2000));
 }
-
-load("./chainspec/polkadot.json", function (contents) {
-    // contents is now set to the contents of "site.com/t.txt"
-    console("loaded chainspec");
-    client.addChain({
-        chainSpec: contents,
-        jsonRpcCallback: (jsonRpcResponse) => {
-            current = jsonRpcResponse;
-            // Called whenever the client emits a response to a JSON-RPC request,
-            // or an incoming JSON-RPC notification.
-            console.log(jsonRpcResponse)
-        }
-    }).then((chain) => {
-        chain.sendJsonRpc('{"jsonrpc":"2.0","id":1,"method":"system_name","params":[]}');
-    });
+console.log(window.wasm.chainspec_string());
+client.addChain({
+    chainSpec: window.wasm.chainspec_string(),
+    jsonRpcCallback: (jsonRpcResponse) => {
+        current = jsonRpcResponse;
+        // Called whenever the client emits a response to a JSON-RPC request,
+        // or an incoming JSON-RPC notification.
+        console.log(jsonRpcResponse)
+    }
+}).then((chain) => {
+    chain.sendJsonRpc('{"jsonrpc":"2.0","id":1,"method":"system_name","params":[]}');
 });
 
 // const chainSpec = fs.readFileSync('./chainspec/polkadot.json', 'utf8');
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn chainspec_string() -> String {
+    // &'static str can't be returned to bindgen yet...
+    // https://github.com/rustwasm/wasm-bindgen/issues/1187
+    include_str!("../chainspecs/polkadot.json").to_string()
+}
+
 fn main() {
     println!("Hello, world!");
 }
